416056338231abb6180fac50c904906f5f921c16
Henry Au <henry.au@moesol.com>
2021-05-25 10:35:25 -0700

    Fix port configuration to use environment variable

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
